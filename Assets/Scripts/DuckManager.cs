b8b856865add1b08abc0d55e540c1127a3b9dbb7
sarahcrichmond <sarah.richmond@medcognition.com>
2020-11-14 15:14:33 -0600

    fixed ui anvas prefb

---
+++ Assets/Scripts/DuckManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckManager : MonoBehaviour
+{
+    [NonSerialized]
+    public int numArchers;
+    [NonSerialized]
+    public int numArtillery;
+    [NonSerialized]
+    public int numWizards;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //left click
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                //need to check if enemy was clicked
+
+                AdultDuck[] adultDucks = FindObjectsOfType<AdultDuck>();
+                foreach (AdultDuck duck in adultDucks)
+                {
+                    duck.SetDestination(hit.point);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1)) //right click to make ducks follow the player
+        {
+            //need to get the selected type first
+            AdultDuck[] adultDucks = FindObjectsOfType<AdultDuck>();
+            foreach (AdultDuck duck in adultDucks)
+            {
+                duck.SetDestination(this.gameObject.transform.position);
+            }
+
+        }
+    }
+
+
+}
