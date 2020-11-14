3310cb993b04dcab0abbc2cfb86647965060fbc9
Gene Tigner <gene.tigner@medcognition.com>
2020-11-14 09:45:03 -0600

    added terrain to mouse mapping

---
+++ Assets/Scripts/MouseMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMap : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
