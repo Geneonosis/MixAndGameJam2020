c86662f293c0fdee88541191d162dc4e861e667e
sarahcrichmond <sarah.richmond@medcognition.com>
2020-11-14 09:57:19 -0600

    added duck evolutions and item pickups

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class Follower : MonoBehaviour
 {
+    public GameObject redDuck;
+    public GameObject blueDuck;
+    public GameObject greenDuck;
 
     private void OnTriggerStay(Collider other)
     {
@@ -22,6 +25,37 @@ public class Follower : MonoBehaviour
         {
             //No longer following leader, so start wandering around randomly again.
             GetComponent<RandomWander>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PickUpItem>())
+        {
+            //found a pickup item, lets evolve into a adult duckling based on the type of item
+            switch (other.gameObject.GetComponent<PickUpItem>().myType)
+            {
+                case PickUpItem.itemType.blue:
+                    Instantiate(blueDuck, this.transform.position, Quaternion.identity);
+                    Destroy(this.gameObject);
+                    Destroy(other.gameObject);
+                    break;
+                case PickUpItem.itemType.green:
+                    Instantiate(greenDuck, this.transform.position, Quaternion.identity);
+                    Destroy(this.gameObject);
+                    Destroy(other.gameObject);
+                    break;
+                case PickUpItem.itemType.red:
+                    Instantiate(redDuck, this.transform.position, Quaternion.identity);
+                    Destroy(this.gameObject);
+                    Destroy(other.gameObject);
+                    break;
+                default:
+                    Debug.Log("did not find a valid item type on pick up");
+                    break;
+            }
+            GetComponent<NavMeshAgent>().SetDestination(other.gameObject.transform.position);
+            GetComponent<RandomWander>().enabled = false;
         }
     }
 }
---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    //pick up item has a type which determines what the duckling should evolve into
+    public enum itemType { red, blue, green };
+    public itemType myType;
+
+}
