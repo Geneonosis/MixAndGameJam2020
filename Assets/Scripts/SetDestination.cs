48ac6463e49c0c8930dd115eb45f492d0367c753
sarahcrichmond <sarah.richmond@medcognition.com>
2020-11-13 19:45:48 -0600

    fixed genes scene, added duckling wander script

---
+++ Assets/Scripts/SetDestination.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestination : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }
+
+}
