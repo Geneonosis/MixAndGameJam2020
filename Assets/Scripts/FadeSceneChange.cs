3ca82ba1e912bcb6929f2009f6948eabb37d54b1
Gene Tigner <gene.tigner@medcognition.com>
2020-11-13 19:40:33 -0600

    created a script that has a specific use case for transitioning between scenes on enable for animations

---
+++ Assets/Scripts/FadeSceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Used to change scenes at the end of a fade animation
+/// Usage: add to component and in the animator assign to enable at end of animation
+/// </summary>
+public class FadeSceneChange : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        loadTitleScene();
+    }
+
+    public void loadTitleScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
