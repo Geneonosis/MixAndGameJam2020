74def83c0f1f3cc625f312b41c10dd178a463e0b
Gene Tigner <gene.tigner@medcognition.com>
2020-11-14 10:43:11 -0600

    added the unity standard assets library added code to allow for point and click, and to place placeholders added a projection for mouse location

--- Assets/Scripts/MouseMap.cs
+++ Assets/Scripts/MouseMap.cs
@@ -4,13 +4,57 @@ using UnityEngine;
 
 public class MouseMap : MonoBehaviour
 {
+
+    public GameObject placeholder = null;
+    public GameObject lastMarker = null;
+
+    private void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// creates a placeholder at the position on the terrain the mouse is
+    /// </summary>
+    private void RenderPlaceholder()
+    {
+        if (lastMarker)
+        {
+            Destroy(lastMarker);
+        }
+
+        GameObject ph = Instantiate(this.placeholder);
+        ph.transform.position = TerrainToMousePosition();
+        this.lastMarker = ph;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RenderPlaceholder();
+        }
+    }
+
     private void FixedUpdate()
+    {
+        transform.position = TerrainToMousePosition();
+
+    }
+
+    /// <summary>
+    /// function for finding the mouse position in 3d space
+    /// </summary>
+    /// <returns> mouse position on the terrain </returns>
+    private Vector3 TerrainToMousePosition()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, 1000))
+        if (Physics.Raycast(ray, out hit, 1000))
         {
-            transform.position = hit.point;
+            return hit.point;
         }
+
+        return hit.point;
     }
 }
