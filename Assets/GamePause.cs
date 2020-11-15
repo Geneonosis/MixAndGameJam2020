9ffb6c70a73b4952585e0818fa7b0f57cacc678f
Gene Tigner <gene.tigner@medcognition.com>
2020-11-15 10:50:56 -0600

    added camera controls for pausing and unpausing the game

---
+++ Assets/GamePause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public GameObject EscapeMenu;
+    public bool pause = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+            if (pause)
+            {
+                this.EscapeMenu.SetActive(pause);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                this.EscapeMenu.SetActive(pause);
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
