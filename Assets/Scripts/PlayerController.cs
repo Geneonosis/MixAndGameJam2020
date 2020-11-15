fcc49858b4080f06f4971b5518ad3b0287b300ea
Gene Tigner <gene.tigner@medcognition.com>
2020-11-15 09:43:47 -0600

    animated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
     public float movementSpeed = 5;
     public GameObject myExplosion;
+    public Animator animator;
     
 
     public void Update()
@@ -14,6 +15,15 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
 
         transform.position += (transform.forward * Input.GetAxis("Vertical")) * Time.deltaTime * movementSpeed;
+
+        if ((Input.GetAxis("Vertical") != 0) || (Input.GetAxis("Horizontal") != 0))
+        {
+            this.animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            this.animator.SetBool("isWalking", false);
+        }
     }
 
     
