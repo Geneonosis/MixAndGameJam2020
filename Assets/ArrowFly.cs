e6e9e5ee197e60ab1b29dcefdf8a3047ebc92207
sarahcrichmond <sarah.richmond@medcognition.com>
2020-11-14 21:16:17 -0600

    added health script

--- Assets/ArrowFly.cs
+++ Assets/ArrowFly.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ArrowFly : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float movementSpeed = 20;
+    public float damageAmount;
     // Update is called once per frame
     void Update()
     {
+        //fly off in facing forward direction
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        //if detect collider, delete self
+        //if detect enemy collider, hurt enemy
+        //delete self after some time so dont go off forever
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("hit " + collision.gameObject.name);
+        if (collision.gameObject.GetComponent<Health>())
+            collision.gameObject.GetComponent<Health>().TakeDamage(damageAmount);
+        Destroy(this.gameObject);
         
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("hit " + other.gameObject.name);
+    //    Destroy(this.gameObject);
+    //}
+
+
+    public void Start()
+    {
+        Destroy(this.gameObject, 10);
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float healthLevel;
+
+    public void TakeDamage(float amount)
+    {
+        healthLevel -= amount;
+        if (healthLevel <= 0)
+            Destroy(this.gameObject);
+    }
+
+
+}
