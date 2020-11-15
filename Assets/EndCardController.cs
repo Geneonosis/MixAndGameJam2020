8573812380a65e109ec0be03af717e5dd8460477
Gene Tigner <gene.tigner@medcognition.com>
2020-11-15 08:32:03 -0600

    added more assets and fixed end card flow

---
+++ Assets/EndCardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCardController : MonoBehaviour
+{
+    /// <summary>
+    /// Reloads the current scene
+    /// </summary>
+    public void onRetryPress()
+    {
+        //retry will only work when the game is built
+        Debug.Log("scene needs to exist in build list");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Loads the Main Menu
+    /// </summary>
+    public void onGetTheDuckOuttaHere()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/EndCardEnabler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCardEnabler : MonoBehaviour
+{
+    public GameObject EndCard;
+    /// <summary>
+    /// use this for the Mask Animation (add event) option 
+    /// at the end of it's animation
+    /// </summary>
+    public void enableEndCard()
+    {
+        this.EndCard.SetActive(true);
+    }
+}
