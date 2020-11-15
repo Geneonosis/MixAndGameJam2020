37a9e6393e16b0f979cf390f8499bf028d078fa7
Gene Tigner <gene.tigner@medcognition.com>
2020-11-15 11:16:50 -0600

    tutorial popup

---
+++ Assets/InstructionPause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionPause : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/PauseControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseControls : MonoBehaviour
+{
+    public GameObject tutorial;
+    
+    public void onRetryPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void onTutorialPopup()
+    {
+        Time.timeScale = 0;
+        tutorial.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnQuitGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
