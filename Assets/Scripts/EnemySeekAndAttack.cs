b2cbe490ab017cc5e1dac36bb5466049f463bfb4
sarahcrichmond <sarah.richmond@medcognition.com>
2020-11-15 10:07:39 -0600

    updated level terrain

--- Assets/Scripts/EnemySeekAndAttack.cs
+++ Assets/Scripts/EnemySeekAndAttack.cs
@@ -5,29 +5,89 @@ using UnityEngine.AI;
 
 public class EnemySeekAndAttack : MonoBehaviour
 {
-    //check if ducklings in range, if they are, turn off wander, and seek to destroy target
 
-    //public void FixedUpdate()
+    public void FixedUpdate()
+    {
+        Attack();
+    }
+
+    private float _t = 0;
+    public GameObject target;
+    public float sightRange = 3;
+    public float attackRange = 1;
+    private NavMeshAgent agent;
+    public float fireRate = 1;
+    public float damageAmount = 2;
+
+    public void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }   
+    public void Attack()
+    {
+
+        if (target != null)
+        {
+            // need the definition on how this player attacks? Ranges? etc? damages? arch visibility? etc.
+            if (Vector3.Distance(this.target.transform.position, this.transform.position) < attackRange)
+            {
+                this.transform.LookAt(target.transform.position);
+                this.agent.isStopped = true;
+                // begin attacking the target.
+                _t += Time.deltaTime / fireRate;
+                if (_t > 1)
+                {
+                    Debug.Log("enemy attacking target " + target.gameObject.name);
+                    _t = 0;
+                    target.GetComponent<Health>().TakeDamage(damageAmount);
+                }
+
+            }
+            else //target not in attack range, move towards it
+            {
+                Debug.Log("enemy moving towards target " + target.gameObject.name);
+                this.agent.SetDestination(target.transform.position);
+            }
+        }
+        else
+        {
+            //seek targets
+            Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, sightRange);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.gameObject.GetComponent<Follower>()
+                    || hitCollider.gameObject.GetComponent<ArtilleryDuck>()
+                    || hitCollider.gameObject.GetComponent<MageDuck>()
+                    || hitCollider.gameObject.GetComponent<ArcheryDuck>()
+                    || hitCollider.gameObject.GetComponent<PlayerController>())
+                { //set the target
+                    Debug.Log("enemy found target in sight range " + hitCollider.gameObject.name);
+                    target = hitCollider.gameObject;
+                    break;
+                }
+
+            }
+
+        }
+    }
+
+    //private void OnTriggerStay(Collider other)
     //{
-        
+    //    Debug.Log("en on triger entered");
+    //    if (other.gameObject.GetComponent<Follower>())
+    //    {
+    //        //found a duckling, go attack it!
+    //        GetComponent<NavMeshAgent>().SetDestination(other.gameObject.transform.position);
+    //        GetComponent<RandomWander>().enabled = false;
+    //    }
     //}
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.GetComponent<Follower>())
-        {
-            //found a duckling, go attack it!
-            GetComponent<NavMeshAgent>().SetDestination(other.gameObject.transform.position);
-            GetComponent<RandomWander>().enabled = false;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.GetComponent<Follower>())
-        {
-            //No longer following duckling, so start wandering around randomly again.
-            GetComponent<RandomWander>().enabled = true;
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.GetComponent<Follower>())
+    //    {
+    //        //No longer following duckling, so start wandering around randomly again.
+    //        GetComponent<RandomWander>().enabled = true;
+    //    }
+    //}
 }
